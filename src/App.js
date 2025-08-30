f45f8cadcbd6a19fb60ed1e6ad969f33fc6b5e14
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-30 01:27:54 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import KanbanBoard from './KanbanBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kanban Board</h1>
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
